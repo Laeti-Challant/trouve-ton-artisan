3f59b48cdbd23c6ada01b95261265dde69904abf
Laeya <l.challant@gmail.com>
2025-05-19 20:51:02 +0200

    style: refactor header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   Button,
-  Container,
   Form,
   FormControl,
   InputGroup,
@@ -14,62 +13,44 @@ import Logo from "../common/Logo";
 
 export default function Header() {
   return (
-    <header className="px-5">
-      <Navbar expand="lg">
+    <header className="px-2 px-md-5">
+      <Navbar expand="lg" className="d-flex justify-content-around">
         <Navbar.Brand as={Link} to="/">
           <Logo />
         </Navbar.Brand>
-        <Container>
-          <Form className="d-flex me-auto">
-            <InputGroup>
-              <FormControl
-                type="search"
-                placeholder="Rechercher"
-                aria-label="Search"
-                className="search-input"
-              />
-              <Button variant="outline-secondary">
-                <FontAwesomeIcon icon={faMagnifyingGlass} />
-              </Button>
-            </InputGroup>
-          </Form>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse
-            id="basic-navbar-nav"
-            // className="d-flex justify-content-end"
-          >
-            <Nav className="ms-auto">
-              <Nav.Link
-                className="fs-5 fw-bold text-secondary"
-                as={Link}
-                to="/"
-              >
-                Alimentation
-              </Nav.Link>
-              <Nav.Link
-                className="fs-5 fw-bold text-secondary"
-                as={Link}
-                to="/"
-              >
-                Batîment
-              </Nav.Link>
-              <Nav.Link
-                className="fs-5 fw-bold text-secondary"
-                as={Link}
-                to="/"
-              >
-                Fabrication
-              </Nav.Link>
-              <Nav.Link
-                className="fs-5 fw-bold text-secondary"
-                as={Link}
-                to="/"
-              >
-                Services
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
+        <Form className="d-flex me-auto">
+          <InputGroup>
+            <FormControl
+              type="search"
+              placeholder="Rechercher"
+              aria-label="Search"
+              className="search-input"
+            />
+            <Button variant="outline-secondary">
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </Button>
+          </InputGroup>
+        </Form>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          // className="d-flex justify-content-end"
+        >
+          <Nav className="ms-auto">
+            <Nav.Link className="fs-5 fw-bold text-secondary" as={Link} to="/">
+              Alimentation
+            </Nav.Link>
+            <Nav.Link className="fs-5 fw-bold text-secondary" as={Link} to="/">
+              Batîment
+            </Nav.Link>
+            <Nav.Link className="fs-5 fw-bold text-secondary" as={Link} to="/">
+              Fabrication
+            </Nav.Link>
+            <Nav.Link className="fs-5 fw-bold text-secondary" as={Link} to="/">
+              Services
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </header>
   );

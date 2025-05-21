508a5c01fcf346cbb8dd5268787ac1eba13624f6
Laeya <l.challant@gmail.com>
2025-05-21 10:16:10 +0200

    feat:create artisan interface + create ArtisanCard + complete Home with lorem in container and carousel

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,8 +1,73 @@
+import { Carousel, CarouselItem, Container } from "react-bootstrap";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <p>Welcome to the home page!</p>
-    </div>
+    <main>
+      <Container>
+        <h1 className="text-primary">Trouve ton artisan</h1>
+        <h2>Comment ça marche</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+          nulla error eligendi veniam ea dolorum rem, odit aut blanditiis porro
+          fuga deleniti autem voluptatem esse dolore officiis quidem nobis
+          laudantium sed. Sit tenetur quisquam error fugiat deserunt voluptatum,
+          minima, quae iste eveniet quia facilis officia sapiente fugit facere,
+          itaque magnam.
+        </p>
+      </Container>
+      <Carousel>
+        <div className="carousel_indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div className="carousel-inner">
+          <CarouselItem className="active"></CarouselItem>
+          <CarouselItem></CarouselItem>
+          <CarouselItem></CarouselItem>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </Carousel>
+    </main>
   );
 }

42e8aad86a0f7bf29b34568b4d7bd99c444e6a9a
Laeti-Challant <l.challant@gmail.com>
2025-05-29 18:23:14 +0200

    feat: implement PageNotFound component and update routing; enhance MainLayout styles

---
+++ src/components/pages/PageNotFound.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+export default function PageNotFound() {
+  return (
+    <>
+      <title>Page non trouvée</title>
+      <meta
+        name="description"
+        content="La page que vous cherchez n'existe pas."
+      />
+      <main className="text-center my-5">
+        <h1 className="display-4">404 - Page non trouvée</h1>
+        <p className="lead">Désolé, la page que vous cherchez n'existe pas.</p>
+        <Link to="/" className="btn btn-primary">
+          Retour à l'accueil
+        </Link>
+      </main>
+    </>
+  );
+}
